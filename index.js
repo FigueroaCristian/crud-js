fd6f48915549f2a0417e55fdd419a642a99028f3
Cristian <crisitandurangofigueroa@gmail.com>
2023-02-21 01:26:53 +0000

    functionality to add new tables with data

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-document.addEventListener('DOMContentLoaded', ()=> {
-   const btn = document.getElementById('add'); 
-   const title = document.getElementById('title');
-   const description = document.getElementById('description');
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.getElementById("add");
+  const title = document.getElementById("title");
+  const description = document.getElementById("description");
+  const alert = document.getElementById("alert");
+  const table = document.getElementById("table");
 
-   btn.addEventListener('click', ()=> {
-      AddTodo();
-   });
-   const AddTodo = ()=> {
-      if(title.value === '' || description.value === '') {
-         console.error('los datos son requeridos');
-      }else {
-         console.log('datos enviados');
-      }
-   }
+  btn.addEventListener("click", () => {
+    AddTodo();
+  });
+  const AddTodo = () => {
+    if (title.value === "" || description.value === "") {
+      alert.classList.remove("d-none");
+      alert.innerHTML = "los datos son requerdios!";
+      return;
+    }
+    alert.classList.add("d-none");
+    const row = table.insertRow();
+    row.innerHTML = `
+    <td>
+    ${title.value}
+  </td>
+  <td>
+    ${description.value}
+  </td>
+  <td class="text-center">
+    <input type="checkbox">
+  </td>
+  <td class="text-right">
+    <button class="btn btn-primary mb-1">
+      <i class="fa fa-pencil"></i>
+    </button>
+    <button class="btn btn-danger mb-1 ml-1">
+      <i class="fa fa-trash"></i>
+    </button>
+  </td>
+    `;
+  };
 });
