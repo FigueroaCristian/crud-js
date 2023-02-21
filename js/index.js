5349e71c519051e51b5645aaac66412b237d47ca
Cristian <crisitandurangofigueroa@gmail.com>
2023-02-21 14:00:49 +0000

    main file that starts crud

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import View from './view.js';
+import Model from './model.js';
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+ const view = new View();
+ const model = new Model();
+ view.setModel(model);
+ model.getTodos(view);
+})
