8a6dbd03aeafb5fcdfa4c1910634564c17a86585
Cristian <crisitandurangofigueroa@gmail.com>
2023-02-21 15:40:33 +0000

    functionality to remove all todos

--- js/model.js
+++ js/model.js
@@ -3,12 +3,26 @@ export default class Model {
  constructor() {
   this.view = null;
   this.todos = [];
+  this.currentId = 1;
  }
  setTodo(view) {
   this.view = view;
  }
 
- getTodos(tile,description) {
-  console.log(tile,description);
+
+ getTodos(title,description) {
+  const todo = {
+   id: this.currentId++,
+   title,
+   description,
+   completed: false
+  }
+  this.todos.push(todo);
+  console.log(this.todos);
+  return {...todo};
+ }
+ removeTodo(id) {
+  const Index = this.todos.findIndex(todo => todo.id === id);
+  this.todos.slice(Index, 1);
  }
 }
