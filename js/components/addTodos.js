93ccaef3f8dd176fac63be9d5caf4f5d1822153c
Cristian <crisitandurangofigueroa@gmail.com>
2023-02-21 16:49:32 +0000

    alert functionality

--- js/components/addTodos.js
+++ js/components/addTodos.js
@@ -1,13 +1,16 @@
+import Alert from './Alert.js';
+
 export default class AddTodoForm {
  constructor() {
    this.title = document.getElementById("title");
    this.description = document.getElementById("description");
    this.btn = document.getElementById("add");
+   this.alert = new Alert("los datos son requeridos");
  }
  onClick(callback) {
   this.btn.onclick = ()=> {
    if(this.title.value === '' || this.description.value === '') {
-    console.error('los datos son requeridos');
+    this.alert.show("los datos son requeridos!");
    }else {
     callback(this.title.value , this.description.value);
    }
